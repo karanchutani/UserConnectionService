a641443f7b46af6e4a48e7d569f658f353dac670
karan <karanchutani@qainfotech.com>
2020-10-05 00:13:26 +0530

    Adding main class

---
+++ src/main/java/com/assignment/AstrotalkAssignment/AstrotalkAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.AstrotalkAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrotalkAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrotalkAssignmentApplication.class, args);
+	}
+
+}
